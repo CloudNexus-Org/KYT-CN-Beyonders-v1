cc60ddabd8c7e7111c3d28bc437cbebf86378028
Md Talib <talib>
2026-04-08 16:39:47 +0530

    feat: pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/KYT-CN-Beyonders/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/KYT-CN-Beyonders-v1/' : '/',
   server: {
     port: 3000,
     host: true
